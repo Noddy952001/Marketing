da5872836912241cee1760bff9221b5f359dc581
vikas <vikasnwrdr2001@gmail.com>
2022-09-16 12:50:25 +0530

    add register or login funtionlity

---
+++ Backend/index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+var cors = require('cors')
+const connect = require("./src/configs/db");
+
+const  {register,login}=require("./src/controllers/auth.user.controller")
+
+
+const app = express();
+app.use(cors({ origin:"*"}))
+
+
+app.use(express.json());
+app.post("/register",register)
+app.post("/login" ,login)
+
+// app.use("/booking" , bookingControler)
+// app.use("/users", userController);
+
+app.set("view engine","hbs")
+app.get("/",(req,res)=>{
+  res.render("index")
+})
+
+const PORT=process.env.PORT || 2345
+
+app.listen(PORT, async () => {
+  try {
+    await connect();
+  } catch (err) {
+    console.error(err.message);
+  }
+  console.log("listening on port 2345");
+});
